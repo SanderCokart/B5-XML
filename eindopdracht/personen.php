811e5df0db50e8cf1a1a2b2a8fb6fd0c5199d70a
Sander Cokart <cokart32@gmail.com>
2020-12-15 20:12:46 +0100

    eindopdracht af

---
+++ eindopdracht/personen.php
@@ -0,0 +1,27 @@
+<?php
+$xml = simplexml_load_file('./personen.xml');
+echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>';
+echo '<table class="table" border="true">';
+echo '<thead><tr>';
+echo '<td>Voornaam</td>';
+echo '<td>Achternaam</td>';
+echo '<td>Geslacht</td>';
+echo '<td>Geboortedatum</td>';
+echo '<td>Lengte</td>';
+echo '<td>Gewicht</td>';
+echo '<td>Haarkleur</td>';
+echo '</tr></thead>';
+foreach ($xml as $persoon) {
+    echo '<tr>';
+    echo '<td>' . $persoon->voornaam . '</td>';
+    echo '<td>' . $persoon->achternaam . '</td>';
+    echo '<td>' . $persoon->geslacht . '</td>';
+    echo '<td>' . $persoon->geboortedatum . '</td>';
+    echo '<td>' . $persoon->lengte_in_cm . '</td>';
+    echo '<td>' . $persoon->gewicht_in_kg . '</td>';
+    echo '<td>' . $persoon->haarkleur . '</td>';
+    echo '</tr>';
+}
+echo "</table>";
+echo "<a href='personen.xml'>View XML in browser.</a>";
